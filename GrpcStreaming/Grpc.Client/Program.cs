225c88bf9cecbf664c1e394719a010f4bcaa5162
Faraz Faheem <faraz.faheem@hotmail.com>
2020-10-11 19:37:07 +0800

    Grps Streaming

---
+++ GrpcStreaming/Grpc.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using Grpc.Server.Grpc.Contracts;
+using System;
+
+namespace Grpc.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Sensor.SensorClient(channel);
+            var response = client.GetSignal(new SensorRequest());
+            Console.WriteLine($"Response:{response.Name}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GrpcStreaming/Grpc.Server/Grpc/SensorService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using Grpc.Server.Grpc.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grpc.Server.Grpc
+{
+    public class SensorService : Sensor.SensorBase
+    {
+        public override Task<SensorResponse> GetSignal(SensorRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new SensorResponse() { Name = "MySensor From Server" });
+        }
+    }
+
+}
